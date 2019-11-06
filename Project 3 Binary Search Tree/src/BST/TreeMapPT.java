3346736235f0867492156db203b68715a28182aa
Nathan <31359612+neerb@users.noreply.github.com>
2019-11-06 00:27:48 -0600

    New Files Added

--- Project 3 Binary Search Tree/src/BST/TreeMapPT.java
+++ Project 3 Binary Search Tree/src/BST/TreeMapPT.java
@@ -2,30 +2,36 @@ package BST;
 
 import java.util.Random;
 
-public class TreeMapPT implements SortedSetPT {
+public class TreeMapPT implements SortedSetPT
+{
 	MapEntryPT root;
 
 	@Override
-	public void get(int key) {
+	public void get(int key)
+	{
 		String value = getValue(key, root);
 
 		System.out.println("Found key value: " + value);
 	}
 
-	String getValue(int key, MapEntryPT root) {
-		if (root != null) {
+	String getValue(int key, MapEntryPT root)
+	{
+		if (root != null)
+		{
 			if (root.key == key)
 				return root.value;
 		}
 
-		if (root.left != null) {
+		if (root.left != null)
+		{
 			if (root.left.compareTo(new MapEntryPT(key)) == 1)
 				return root.left.value;
 			else
 				return getValue(key, root.left);
 		}
 
-		if (root.right != null) {
+		if (root.right != null)
+		{
 			if (root.right.compareTo(new MapEntryPT(key)) == 1)
 				return root.right.value;
 			else
@@ -36,12 +42,15 @@ public class TreeMapPT implements SortedSetPT {
 	}
 
 	@Override
-	public void put(int key, String value) {
+	public void put(int key, String value)
+	{
 		root = newEntry(root, key, value);
 	}
 
-	MapEntryPT newEntry(MapEntryPT root, int key, String value) {
-		if (root == null) {
+	MapEntryPT newEntry(MapEntryPT root, int key, String value)
+	{
+		if (root == null)
+		{
 			root = new MapEntryPT(key, value);
 			return root;
 		}
@@ -54,36 +63,30 @@ public class TreeMapPT implements SortedSetPT {
 		return root;
 	}
 
-	static String randomizedValueEntry() {
-		char[] chars = new char[] { '!', '@', '#', '$', '%', '^', '&', '*' };
-		String newStr = "";
-		Random rand = new Random();
-
-		for (int i = 0; i < rand.nextInt(10) + 5; i++)
-			newStr += chars[rand.nextInt(chars.length)];
-
-		return newStr;
-	}
-
 	@Override
-	public void removeKey(int key) {
+	public void removeKey(int key)
+	{
 		keyRemoval(root, key);
 	}
 
-	void keyRemoval(MapEntryPT root, int key) {
-		if (root != null) {
+	void keyRemoval(MapEntryPT root, int key)
+	{
+		if (root != null)
+		{
 			if (root.key == key)
 				root.key = -1;
 		}
 
-		if (root.left != null) {
+		if (root.left != null)
+		{
 			if (root.left.compareTo(new MapEntryPT(key)) == 1)
 				root.left.key = -1;
 			else
 				keyRemoval(root.left, key);
 		}
 
-		if (root.right != null) {
+		if (root.right != null)
+		{
 			if (root.right.compareTo(new MapEntryPT(key)) == 1)
 				root.right.key = -1;
 			else
@@ -92,15 +95,20 @@ public class TreeMapPT implements SortedSetPT {
 	}
 
 	@Override
-	public void print() {
+	public void print()
+	{
 		printSorted(root);
 	}
 
-	void printSorted(MapEntryPT root) {
-		if (root != null) {
+	void printSorted(MapEntryPT root)
+	{
+		if (root != null)
+		{
 			printSorted(root.left);
+			
 			if (root.key != -1)
 				System.out.println(root.key + " with value " + root.value);
+			
 			printSorted(root.right);
 		}
 	}
